4e7fcc033bb63e82bff4a161bd24c94bdb14e429
dani <danifromecuador@gmail.com>
2024-02-04 13:20:51 -0500

    Create TODO list for main golas

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -1,15 +1,31 @@
-const DailyGoals = () => {
+import { useState } from "react";
+
+export const DailyGoals = () => {
+  const [mainGoalsList, setMainGoalsList] = useState([]);
+  const [secondaryGoalsList, setSecondaryGoalsList] = useState([]);
+  const [input, setInput] = useState('');
+  const [secondayGoalInput, setSecondaryGoalInput] = useState('');
+
   return (
-    <div>
+    <div className="daily-goals">
       <h1>Daily Goals</h1>
-      <p>Here are your daily goals:</p>
-      <ul>
-        <li>Exercise</li>
-        <li>Read</li>
-        <li>Write</li>
-      </ul>
+      <div className="main-goals">
+        <h2>Dead or alive goals:</h2>
+        <ul>
+          {mainGoalsList.map((goal, index) => {
+            return <li key={index}>{goal}</li>
+          })}
+        </ul>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => {
+          setMainGoalsList([...mainGoalsList, input]);
+          setInput('');
+        }} >Add</button>
+      </div>
     </div>
   );
 }
-
-export default DailyGoals;
