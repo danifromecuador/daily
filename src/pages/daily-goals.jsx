8be66be1bc6df2c8a23cb4b44b98ac2d4ee42af5
dani <danifromecuador@gmail.com>
2024-02-08 15:12:35 -0500

    Save goals to localStorage

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react";
 
 export const DailyGoals = () => {
   const [input, setInput] = useState("")  
-  const [goals, setGoals] = useState(JSON.parse(localStorage.getItem('mainGoalsList')) || [])
+  const [goals, setGoals] = useState(JSON.parse(localStorage.getItem('goals')) || [])
   // console.log("goals are: " + goals[0]);
-  // useEffect(() => { console.log("input is: " + input) }, [input]);
-  // useEffect(() => { console.log("goals are: " + goals) }, [goals]):
+  useEffect(() => { console.log("input is: " + input) }, [input]);
+  useEffect(() => { console.log("goals are: " + goals) }, [goals]);
+
+  // update localStorage whenever goals list changes
+  useEffect(()=>{
+    localStorage.setItem('goals', JSON.stringify(goals))
+  }, [goals])
 
   const manageAddGoalBtn = () => {
     if (input) {
