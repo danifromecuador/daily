0f392491089539423d8941059b1302572da2ff6e
dani <danifromecuador@gmail.com>
2024-02-08 15:40:12 -0500

    Improve code readability

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -4,40 +4,23 @@ export const DailyGoals = () => {
   const [input, setInput] = useState("")
   const [goals, setGoals] = useState(JSON.parse(localStorage.getItem('goals')) || [])
 
-  // update (or delete) localStorage whenever goals list changes
-  useEffect(() => {
-    localStorage.setItem('goals', JSON.stringify(goals));
-  }, [goals]);
+  useEffect(() => {  // update (or delete) localStorage whenever goals list changes
+    localStorage.setItem('goals', JSON.stringify(goals))
+  }, [goals])
 
   const manageAddGoalBtn = () => {
     if (input) {
-      setGoals([...goals, input]);
-      setInput("");
+      setGoals([...goals, input])
+      setInput("")
     }
   }
 
-  const manageDeleteAllGoalsBtn = () => {
-    setGoals([]);
-  }
+  const manageDeleteAllGoalsBtn = () => setGoals([])
 
-  return (<>
-    <input
-      type="text"
-      value={input}
-      onChange={(e) => { setInput(e.target.value) }}
-    />
+  return <>
+    <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
     <button onClick={manageAddGoalBtn} >Add</button>
-    <div>
-      <ul>
-        {
-          goals.map((e)=>{
-            return ( <div>{e}</div> )
-          })
-        }
-      </ul>
-    </div>
-    <div>
-      <button onClick={manageDeleteAllGoalsBtn} >Delete All Goals</button>
-    </div>
-  </>)
+    <ul>{goals.map((e, i) => <div key={i}>{e}</div>)}</ul>
+    <button onClick={manageDeleteAllGoalsBtn} >Delete All Goals</button>
+  </>
 }
