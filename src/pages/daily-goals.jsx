f6b07861500161654b41bfc75d2df79892a7889e
dani <danifromecuador@gmail.com>
2024-02-08 14:57:38 -0500

    Add a new goal to goals array

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -1,5 +1,26 @@
 import { useState, useEffect } from "react";
 
 export const DailyGoals = () => {
-  return <></>
+  const [input, setInput] = useState("")
+  const [goals, setGoals] = useState([])
+  
+  useEffect(()=>{console.log("input is: " + input)},[input]);
+  useEffect(()=>{console.log("goals are: " + goals)}, [goals])
+
+  const manageAddGoalBtn = () => {
+    setGoals([...goals, input]);
+    setInput('');
+  }
+
+  return (<>
+    <input
+      type="text"
+      value={input}
+      onChange={(e)=>{setInput(e.target.value)}}
+    />
+    <button onClick={manageAddGoalBtn} >Add</button>
+  </>)
+
+
+
 }
