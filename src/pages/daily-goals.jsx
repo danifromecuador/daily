c4a804ffae31832b7c9b4f2b29e2eab1b1cb20ec
dani <danifromecuador@gmail.com>
2024-02-08 15:30:15 -0500

    Delete all goals

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
 
 export const DailyGoals = () => {
-  const [input, setInput] = useState("")  
+  const [input, setInput] = useState("")
   const [goals, setGoals] = useState(JSON.parse(localStorage.getItem('goals')) || [])
-  // console.log("goals are: " + goals[0]);
-  useEffect(() => { console.log("input is: " + input) }, [input]);
-  useEffect(() => { console.log("goals are: " + goals) }, [goals]);
 
-  // update localStorage whenever goals list changes
-  useEffect(()=>{
-    localStorage.setItem('goals', JSON.stringify(goals))
-  }, [goals])
+  // update (or delete) localStorage whenever goals list changes
+  useEffect(() => {
+    localStorage.setItem('goals', JSON.stringify(goals));
+  }, [goals]);
 
   const manageAddGoalBtn = () => {
     if (input) {
       setGoals([...goals, input]);
-      setInput('');
+      setInput("");
     }
+  }
+
+  const manageDeleteAllGoalsBtn = () => {
+    setGoals([]);
   }
 
   return (<>
@@ -26,8 +27,8 @@ export const DailyGoals = () => {
       onChange={(e) => { setInput(e.target.value) }}
     />
     <button onClick={manageAddGoalBtn} >Add</button>
+    <div>
+      <button onClick={manageDeleteAllGoalsBtn} >Delete All Goals</button>
+    </div>
   </>)
-
-
-
 }
