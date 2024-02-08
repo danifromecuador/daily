ec0b9055cb972a602e7f0c352995a151343b6d08
dani <danifromecuador@gmail.com>
2024-02-08 15:07:30 -0500

    Retrive goals list from localStorage

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 
 export const DailyGoals = () => {
-  const [input, setInput] = useState("")
-  const [goals, setGoals] = useState([])
-
-  useEffect(() => { console.log("input is: " + input) }, [input]);
-  useEffect(() => { console.log("goals are: " + goals) }, [goals])
+  const [input, setInput] = useState("")  
+  const [goals, setGoals] = useState(JSON.parse(localStorage.getItem('mainGoalsList')) || [])
+  // console.log("goals are: " + goals[0]);
+  // useEffect(() => { console.log("input is: " + input) }, [input]);
+  // useEffect(() => { console.log("goals are: " + goals) }, [goals]):
 
   const manageAddGoalBtn = () => {
     if (input) {
