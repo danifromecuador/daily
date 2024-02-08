685e91c04ed6aedd72718aff048fbafff97d23fc
dani <danifromecuador@gmail.com>
2024-02-08 15:00:17 -0500

    Avoid saving null values

--- src/pages/daily-goals.jsx
+++ src/pages/daily-goals.jsx
@@ -3,20 +3,22 @@ import { useState, useEffect } from "react";
 export const DailyGoals = () => {
   const [input, setInput] = useState("")
   const [goals, setGoals] = useState([])
-  
-  useEffect(()=>{console.log("input is: " + input)},[input]);
-  useEffect(()=>{console.log("goals are: " + goals)}, [goals])
+
+  useEffect(() => { console.log("input is: " + input) }, [input]);
+  useEffect(() => { console.log("goals are: " + goals) }, [goals])
 
   const manageAddGoalBtn = () => {
-    setGoals([...goals, input]);
-    setInput('');
+    if (input) {
+      setGoals([...goals, input]);
+      setInput('');
+    }
   }
 
   return (<>
     <input
       type="text"
       value={input}
-      onChange={(e)=>{setInput(e.target.value)}}
+      onChange={(e) => { setInput(e.target.value) }}
     />
     <button onClick={manageAddGoalBtn} >Add</button>
   </>)
